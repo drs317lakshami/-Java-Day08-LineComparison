6fcaf63443fc8e7eb0dcec45e0cd67ee044e2d8e
drs317lakshami <devrajkumar317@gmail.com>
2022-09-13 23:52:54 +0530

    Employee part time wage

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,19 +1,33 @@
 package com.bridgelabz.empwage.linecomparisions;
-
 import java.util.Random;
 
 public class EmpWageComputation {
 	
 	static int wagePerHr = 20;
 	static int fullDayHr = 8;
+	static int partTimeHr = 4;
 	
-public static void main(String[] args) {
-	
-		System.out.println("Welcome To Employee Wage Computation Program");
+	public static void main(String[] args) {
 		
-		DailyWage();
-//		Employee Attendance
-		
+			System.out.println("Welcome To Employee Wage Computation Program");
+			
+			CheckEmployee();
+			
+			DailyWage();
+			
+			PartTimeWage();
+		}
+
+//Employee Wage
+
+	public static int DailyWage() {
+		int dailyWage = wagePerHr * fullDayHr;
+		System.out.println("Employee Daily Wage is = " +dailyWage);
+		return (dailyWage);
+		}
+
+
+	public static int CheckEmployee() {
 		Random rand = new Random();
 	    int random = 3;
 		int randNum = rand.nextInt(random);
@@ -30,14 +44,12 @@ public static void main(String[] args) {
 		default : 
 			System.out.println("Employee is Absent");
 		}
-	}
+		return randNum;
+		}
 
-//Employee Wage
-
-public static int DailyWage() {
-	int dailyWage = wagePerHr * fullDayHr;
-	System.out.println("Employee Daily Wage is = " +dailyWage);
-	return (dailyWage);
-	}
-
+	public static int PartTimeWage() {
+		int partTime = wagePerHr * partTimeHr;
+		System.out.println("Employee Part Time Wage is = " +partTime);
+		return (partTime);
+		}
 }
