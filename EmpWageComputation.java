02249d97eb3c16505950753d3d0c9d6ebd287657
drs317lakshami <devrajkumar317@gmail.com>
2022-09-13 23:48:50 +0530

    Employee daily wage

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -4,30 +4,40 @@ import java.util.Random;
 
 public class EmpWageComputation {
 	
+	static int wagePerHr = 20;
+	static int fullDayHr = 8;
+	
 public static void main(String[] args) {
-		
+	
 		System.out.println("Welcome To Employee Wage Computation Program");
 		
-		CheckEmployee();
-}
+		DailyWage();
+//		Employee Attendance
+		
+		Random rand = new Random();
+	    int random = 3;
+		int randNum = rand.nextInt(random);
+		
+		switch(randNum) {
+		case 1:
+			System.out.println("Employee Is Present");
+			break;
+			
+		case 2: 
+			System.out.println("Employee is present Part time");
+			break;
+			
+		default : 
+			System.out.println("Employee is Absent");
+		}
+	}
 
-public static int CheckEmployee() {
-	Random rand = new Random();
-    int random = 3;
-	int randNum = rand.nextInt(random);
-	
-	switch(randNum) {
-	case 1:
-		System.out.println("Employee Is Present");
-		break;
-		
-	case 2: 
-		System.out.println("Employee is present Part time");
-		break;
-		
-	default : 
-		System.out.println("Employee is Absent");
+//Employee Wage
+
+public static int DailyWage() {
+	int dailyWage = wagePerHr * fullDayHr;
+	System.out.println("Employee Daily Wage is = " +dailyWage);
+	return (dailyWage);
 	}
-	return randNum;
-	}
+
 }
